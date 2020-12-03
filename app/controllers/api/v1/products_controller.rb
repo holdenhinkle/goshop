2b810092ce0151f2add79e9450d6c5abc9a485db
Holden Hinkle <holdenhinkle@gmail.com>
2020-12-03 00:11:50 -0500

    add create action

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -10,6 +10,22 @@ module Api
         product = Product.find(params[:id])
         render json: ProductSerializer.new(product).serializable_hash.to_json
       end
+
+      def create
+        product = Product.new(product_params)
+
+        if product.save
+          render json: ProductSerializer.new(product).serializable_hash.to_json
+        else
+          render json: { error: product.errors.messages }, status: 422
+        end
+      end
+
+      private
+
+      def product_params
+        params.require(:product).permit(:name, :description, :image, :product_type, :regular_price, :sale_price, :inventory_amount, :inventory_unit_type, :is_visable)
+      end
     end
   end
 end
