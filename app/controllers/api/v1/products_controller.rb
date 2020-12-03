9316f016cad3010e5cf79c0b469a1b8555a13b80
Holden Hinkle <holdenhinkle@gmail.com>
2020-12-02 23:47:35 -0500

    create product index action

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,10 @@
 module Api
   module V1
     class ProductsController < ApplicationController
+      def index
+        products = Product.all
+        render json: ProductSerializer.new(products).serializable_hash.to_json
+      end
     end
   end
 end
