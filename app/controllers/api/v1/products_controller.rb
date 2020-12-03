cc0e2a479284786f9586f091b4d4064c04459dfa
Holden Hinkle <holdenhinkle@gmail.com>
2020-12-03 00:22:40 -0500

    create update action

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,14 +1,16 @@
 module Api
   module V1
     class ProductsController < ApplicationController
+      before_action :set_product, only: [:show, :update, :delete]
+
       def index
         products = Product.all
+        
         render json: ProductSerializer.new(products).serializable_hash.to_json
       end
 
       def show
-        product = Product.find(params[:id])
-        render json: ProductSerializer.new(product).serializable_hash.to_json
+        render json: ProductSerializer.new(@product).serializable_hash.to_json
       end
 
       def create
@@ -21,10 +23,31 @@ module Api
         end
       end
 
+      def update
+        if @product.update(product_params)
+          render json: ProductSerializer.new(@product).serializable_hash.to_json
+        else
+          render json: { error: @product.errors.messages }, status: 422
+        end
+      end
+
       private
 
+      def set_product
+        @product = Product.find(params[:id])
+      end
+
       def product_params
-        params.require(:product).permit(:name, :description, :image, :product_type, :regular_price, :sale_price, :inventory_amount, :inventory_unit_type, :is_visable)
+        params.require(:product)
+          .permit(:name,
+                  :description,
+                  :image,
+                  :product_type,
+                  :regular_price,
+                  :sale_price,
+                  :inventory_amount,
+                  :inventory_unit_type,
+                  :is_visable)
       end
     end
   end
