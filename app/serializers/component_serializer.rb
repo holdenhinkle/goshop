6f805c7cf71db16ad035e4996d4e2da2a0df66b4
Holden Hinkle <holdenhinkle@gmail.com>
2021-01-02 22:35:33 -0500

    include product_option attributes when a component is serialized

--- app/serializers/component_serializer.rb
+++ app/serializers/component_serializer.rb
@@ -3,4 +3,8 @@ class ComponentSerializer
   set_key_transform :camel_lower
   attributes :name, :description, :image, :slug, :min_quantity, :max_quantity, :is_enabled
   has_many :product_options, serializer: ProductSerializer
+
+  attribute :product_options do |object|
+    object.product_options.as_json
+  end
 end
