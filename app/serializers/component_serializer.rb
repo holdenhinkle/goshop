f96234c60b21211ff3ac2e9a15ded8769b0afe3d
Holden Hinkle <holdenhinkle@gmail.com>
2021-01-02 22:36:50 -0500

    format attributes in ComponentSerializer

--- app/serializers/component_serializer.rb
+++ app/serializers/component_serializer.rb
@@ -1,7 +1,14 @@
 class ComponentSerializer
   include JSONAPI::Serializer
   set_key_transform :camel_lower
-  attributes :name, :description, :image, :slug, :min_quantity, :max_quantity, :is_enabled
+  attributes :name,
+             :description,
+             :image,
+             :slug,
+             :min_quantity,
+             :max_quantity,
+             :is_enabled
+
   has_many :product_options, serializer: ProductSerializer
 
   attribute :product_options do |object|
