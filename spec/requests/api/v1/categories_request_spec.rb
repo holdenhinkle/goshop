44b0bc3142651e83ca0f2fc443a7503d1580bd0f
Holden Hinkle <holdenhinkle@gmail.com>
2021-01-10 14:26:47 -0500

    #index should respond  with a http status success

--- spec/requests/api/v1/categories_request_spec.rb
+++ spec/requests/api/v1/categories_request_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
+require 'pry'
 
-RSpec.describe "Api::V1::Categories", type: :request do
+RSpec.describe Api::V1::CategoriesController, type: :request do
+  url = 'http://localhost:3000/api/v1/categories/'
 
+  describe "GET #index" do
+    before do
+      Category.create(name: 'category 1', description: 'category 1 description')
+      Category.create(name: 'category 2', description: 'category 2 description')
+      get(url)
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
