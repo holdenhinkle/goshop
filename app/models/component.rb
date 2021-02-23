346100a8b6364a3c9db852546b10ff684fd0e0e1
Holden Hinkle <holdenhinkle@gmail.com>
2021-02-23 12:08:51 -0500

    don't add to options if product already is an option

--- app/models/component.rb
+++ app/models/component.rb
@@ -14,7 +14,13 @@ class Component < ApplicationRecord
   def product_option_ids=(product_option_ids)
     product_option_ids.uniq.each do |id|
       product = Simple.find(id)
-      options << product if product
+      options << product if product && product_not_an_option?(product)
     end
+  end
+
+  private
+  
+  def product_not_an_option?(product)
+    options.none? { |option| option.name == product.name}
   end
 end
