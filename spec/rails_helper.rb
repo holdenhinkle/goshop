e34438ba789ca3abdd93b22208672df66c125a9b
Holden Hinkle <holdenhinkle@gmail.com>
2021-02-23 17:16:33 -0500

    update database_cleaner configuration to match documentation

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,31 +41,14 @@ RSpec.configure do |config|
 
   # configs for the database-cleaner
   config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each, :js => true) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
-  config.before(:all) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:all) do
-    DatabaseCleaner.clean
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
   end
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
