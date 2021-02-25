4dcb2c63624b308120fb854ea98f8fec38b2e965
Holden Hinkle <holdenhinkle@gmail.com>
2021-02-25 14:22:35 -0500

    create spec file for ComponentsController

---
+++ spec/requests/api/v1/components_request_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::ComponentsController, type: :request do
+  let!(:url) { 'http://localhost:3000/api/v1/categories/' }
+
+  describe '#index' do
+    
+  end
+end
