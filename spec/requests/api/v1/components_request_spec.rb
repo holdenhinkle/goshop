80bd0e4ecca860b178fde6ad5a87ed4b3dc05439
Holden Hinkle <holdenhinkle@gmail.com>
2021-02-26 11:06:40 -0500

    update url from categories to components

--- spec/requests/api/v1/components_request_spec.rb
+++ spec/requests/api/v1/components_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::ComponentsController, type: :request do
-  let!(:url) { 'http://localhost:3000/api/v1/categories/' }
+  let!(:url) { 'http://localhost:3000/api/v1/components/' }
 
   describe '#index' do
     
