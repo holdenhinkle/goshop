b905b6b15f18c3bb1560d7b75418efcdd17a4e36
Holden Hinkle <holdenhinkle@gmail.com>
2021-02-26 11:04:20 -0500

    refactor category controller tests using category factory traits

--- spec/requests/api/v1/categories_request_spec.rb
+++ spec/requests/api/v1/categories_request_spec.rb
@@ -111,16 +111,11 @@ RSpec.describe Api::V1::CategoriesController, type: :request do
         name = Faker::Lorem.words(number: 2).map(&:capitalize).join(' ')
 
         2.times do
-          category = {
-            name: name,
-            description: Faker::Lorem.paragraph
-          }
-
+          category = attributes_for(:category, name: name)
           post(url, params: { category: category })
         end
 
         body = JSON.parse(response.body)
-
         expect(body['errors'].count).to eq(1)
         expect(body['errors']['name'].count).to eq(1)
         expect(body['errors']['name'][0]).to eq('has already been taken')
@@ -128,10 +123,7 @@ RSpec.describe Api::V1::CategoriesController, type: :request do
 
       context 'name is missing' do
         before do
-          category = {
-            description: Faker::Lorem.paragraph
-          }
-
+          category = attributes_for(:category, :no_name)
           post(url, params: { category: category })
         end
 
@@ -149,11 +141,8 @@ RSpec.describe Api::V1::CategoriesController, type: :request do
 
       context 'description is missing' do
         before do
-          category = {
-            name: Faker::Lorem.words(number: 2).map(&:capitalize).join(' '),
-          }
-
-          post(url, params: { category: category })          
+          category = attributes_for(:category, :no_description)
+          post(url, params: { category: category })
         end
 
         it 'returns http status 422' do
@@ -173,14 +162,8 @@ RSpec.describe Api::V1::CategoriesController, type: :request do
   describe '#update' do
     context 'valid request' do
       before do
-        category = {
-          name: Faker::Lorem.words(number: 2).map(&:capitalize).join(' '),
-          description: Faker::Lorem.paragraph,
-          image: Faker::Internet.url(host: 'example.com')
-        }
-
+        category = attributes_for(:category, :with_image)
         post(url, params: { category: category })
-
         @id = JSON.parse(response.body)['data']['id']
       end
 
@@ -244,12 +227,7 @@ RSpec.describe Api::V1::CategoriesController, type: :request do
     context 'invalid request' do
       before do
         2.times do |n|
-          category = {
-            name: "Category #{n}",
-            description: Faker::Lorem.paragraph,
-            image: Faker::Internet.url(host: 'example.com')
-          }
-
+          category = attributes_for(:category, name: "Category #{n}")
           post(url, params: { category: category })
         end
 
@@ -271,12 +249,7 @@ RSpec.describe Api::V1::CategoriesController, type: :request do
 
     context 'category does not exist' do
       before do
-        category = {
-          name: Faker::Lorem.words(number: 2).map(&:capitalize).join(' '),
-          description: Faker::Lorem.paragraph,
-          image: Faker::Internet.url(host: 'example.com')
-        }
-
+        category = attributes_for(:category)
         patch(url + '1', params: { category: category })
       end
 
@@ -294,12 +267,7 @@ RSpec.describe Api::V1::CategoriesController, type: :request do
   describe '#destroy' do
     context 'valid request' do
       before do
-        category = {
-          name: Faker::Lorem.words(number: 2).map(&:capitalize).join(' '),
-          description: Faker::Lorem.paragraph,
-          image: Faker::Internet.url(host: 'example.com')
-        }
-
+        category = attributes_for(:category)
         post(url, params: { category: category })
       end
 
@@ -308,7 +276,8 @@ RSpec.describe Api::V1::CategoriesController, type: :request do
 
         expect do
           delete(url + id)
-        end.to change(Category, :count).by(-1)    end
+        end.to change(Category, :count).by(-1)
+      end
 
       it 'deletes the category when the category slug is used as identifying param' do
         slug = JSON.parse(response.body)['data']['attributes']['slug']
