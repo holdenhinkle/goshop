4b1787664fbd1df5974e06f4f3c48fa96f614498
Holden Hinkle <holdenhinkle@gmail.com>
2021-03-01 12:08:53 -0500

    create factory for components

--- spec/factories/components.rb
+++ spec/factories/components.rb
@@ -1,7 +1,19 @@
 FactoryBot.define do
   factory :component do
-    name {}
-    description {}
-    min_quantity {}
+    name { Faker::Lorem.words(number: Random.new.rand(1..3)).join(' ') }
+    description { Faker::Lorem.paragraph }
+    min_quantity { 1 }
   end
+
+  trait :with_image do
+    image { Faker::Internet.url(host: 'example.com') }
+  end
+
+  trait :with_max_quantity do
+    max_quantity { 2 }
+  end
+
+  trait :component_product_options do; end
+
+  trait :product_components do; end
 end
