71f02874784e12e829a98bdfd01e7576f900a3b2
Holden Hinkle <holdenhinkle@gmail.com>
2021-03-01 12:10:02 -0500

    write unit test returns 200 ok for #index action

--- spec/requests/api/v1/components_request_spec.rb
+++ spec/requests/api/v1/components_request_spec.rb
@@ -4,6 +4,13 @@ RSpec.describe Api::V1::ComponentsController, type: :request do
   let!(:url) { 'http://localhost:3000/api/v1/components/' }
 
   describe '#index' do
-    
+    before do
+      2.times { create(:component) }
+      get(url)
+    end
+
+    it 'returns http status 200 OK' do
+      expect(response).to have_http_status(200)
+    end
   end
 end
