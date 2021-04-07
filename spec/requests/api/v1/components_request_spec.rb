26a7f0e40ca0cbd1ca610d817b373e4e105733bf
Holden Hinkle <holdenhinkle@gmail.com>
2021-04-06 20:47:21 -0400

    write unit tests for components #index action

--- spec/requests/api/v1/components_request_spec.rb
+++ spec/requests/api/v1/components_request_spec.rb
@@ -12,5 +12,27 @@ RSpec.describe Api::V1::ComponentsController, type: :request do
     it 'returns http status 200 OK' do
       expect(response).to have_http_status(200)
     end
+
+    it "returns two component objects" do
+      components = JSON.parse(response.body)['data']
+      expect(components.count).to eq(2)
+    end
+
+    it 'renders the correct JSON representation of the existing components' do
+      json_response = JSON.parse(response.body)
+
+      json_response['data'].each do |component|
+        expect(component.keys).to match_array(%w[id type attributes relationships])
+        expect(component['type']).to eq('component')
+        expect(component['attributes'].keys).to match_array(%w[name description image slug minQuantity maxQuantity isEnabled])
+        expect(component['relationships'].keys).to match_array(%w[options])
+        expect(component['relationships']['options'].keys).to match_array(%w[data])
+
+        component['relationships']['options']['data'].each do |data|
+          expect(data.keys).to match_array(%w[id type])
+          expect(data['type']).to eq('product')
+        end
+      end
+    end
   end
 end
