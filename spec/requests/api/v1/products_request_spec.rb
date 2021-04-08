ad9ee22d1e01dbdd407499eab261ab10c071be9e
Holden Hinkle <holdenhinkle@gmail.com>
2021-04-08 07:57:38 -0400

    create context outline for products tests

--- spec/requests/api/v1/products_request_spec.rb
+++ spec/requests/api/v1/products_request_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Api.V1.ProductsController, type: :request do
   let!(:url) { 'http://localhost:3000/api/v1/products/' }
+
+  context 'simple product' do
+    
+  end
+
+  context 'composite product' do
+    context 'with component products' do
+      
+    end
+
+    context 'without component products' do
+      
+    end
+  end
 end
