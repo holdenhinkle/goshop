887201a8e768d12e0f2b2dd49e751e9d9a5d9d78
Holden Hinkle <holdenhinkle@gmail.com>
2021-04-10 11:46:55 -0400

    write product #show tests

--- spec/requests/api/v1/products_request_spec.rb
+++ spec/requests/api/v1/products_request_spec.rb
@@ -39,6 +39,92 @@ RSpec.describe Api::V1::ProductsController, type: :request do
         end
       end
     end
+
+    describe '#show' do
+      let!(:product) { create(:simple_product) }
+    
+      it 'renders the correct JSON representation of the product' do
+        id = product.id.to_s
+        get(url + id)
+    
+        product = JSON.parse(response.body)['data']
+        categories = JSON.parse(response.body)['included']
+    
+        expect(product.keys).to match_array(%w[id type attributes relationships])
+        expect(product['type']).to eq('product')
+        expect(product['attributes'].keys).to match_array(%w[name description image type regularPriceCents salePriceCents inventoryAmount unitOfMeasure isVisible slug])
+        expect(product['relationships'].keys).to match_array(%w[categories])
+        expect(product['relationships']['categories'].keys).to match_array(%w[data])
+    
+        categories.each do |category|
+          expect(category.keys).to match_array(%w[id type attributes])
+          expect(category['type']).to eq('category')
+          expect(category['attributes'].keys).to match_array(%w[name description image slug])
+        end
+      end
+    
+      context 'using product id is used as identifying param' do
+        let!(:id) { product.id.to_s }
+        
+        it 'returns http status 200 OK' do
+          get(url + id)
+          expect(response).to have_http_status(200)
+        end
+    
+        it 'returns expected product' do
+          get(url + id)
+          body = JSON.parse(response.body)
+          expect(body['data']['type']).to eq('product')
+          expect(body['data']['id']).to eq(id)
+        end
+    
+        context "when product doesn't exist" do
+          let!(:bad_id) { (id.to_i + 1).to_s }
+    
+          it 'returns an error' do
+            get(url + bad_id)
+            body = JSON.parse(response.body)
+            expect(body['error']).to eq("The requested product doesn't exist")
+          end
+    
+          it 'returns a 404 status code' do
+            get(url + bad_id)
+            expect(response).to have_http_status(404)
+          end
+        end
+      end
+    
+      context 'using product slug is used as identifying param' do
+        let!(:slug) { product.slug }
+    
+        it 'returns http status 200 OK' do
+          get(url + slug)
+          expect(response).to have_http_status(200)
+        end
+    
+        it 'returns expected product' do
+          get(url + slug)
+          body = JSON.parse(response.body)
+          expect(body['data']['type']).to eq('product')
+          expect(body['data']['attributes']['slug']).to eq(slug)
+        end
+    
+        context "when product doesn't exist" do
+          let!(:bad_slug) { (slug + slug) }
+    
+          it 'returns an error when product does not exist' do
+          get(url + bad_slug)
+            body = JSON.parse(response.body)
+            expect(body['error']).to eq("The requested product doesn't exist")
+          end
+    
+          it 'returns a 404 status code when product does not exist' do
+          get(url + bad_slug)
+            expect(response).to have_http_status(404)
+          end
+        end
+      end
+    end    
   end
 
   context 'composite product' do
