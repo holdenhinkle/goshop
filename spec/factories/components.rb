66c484341c64e39238defbd8e5f43e9dd027a49d
Holden Hinkle <holdenhinkle@gmail.com>
2021-04-13 21:57:35 -0400

    refactor name so name is random and doesn't use Faker #unique

--- spec/factories/components.rb
+++ spec/factories/components.rb
@@ -1,8 +1,19 @@
 FactoryBot.define do
   factory :component do
-    name { Faker::Lorem.words(number: Random.new.rand(1..3)).join(' ') }
+    name do
+      Array.new(Random.new.rand(1..3)).map do |word|
+        Faker::Lorem.characters(number: Random.new.rand(3..6)).capitalize
+      end.join(' ')
+    end
+
     description { Faker::Lorem.paragraph }
     min_quantity { 1 }
+
+    product_option_ids do
+      [create(:simple_product_with_category_ids), create(:simple_product_with_category_ids)].each_with_object([]) do |product, array|
+        array << product.id
+      end
+    end
   end
 
   trait :no_name do 
