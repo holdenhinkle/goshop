95c30c5ae3e99572fb470092be7749e49d19b1b3
Holden Hinkle <holdenhinkle@gmail.com>
2021-04-13 20:17:59 -0400

    create factory traits for composite products

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :product do
-    name { Faker::Lorem.unique.words(number: Random.new.rand(1..3)).join(' ') }
+    name { Faker::Lorem.words(number: Random.new.rand(1..3)).join(' ') }
     description { Faker::Lorem.paragraph }
     regular_price_cents { Faker::Number.between(from: 99, to: 20000) }
     unit_of_measure { 'piece' }
@@ -8,14 +8,39 @@ FactoryBot.define do
 
   factory :simple_product_with_category_ids, parent: :product, class: Simple do
     type { 'Simple' }
+    
     category_ids do
-      [create(:category), create(:category)].each_with_object([]) { |category, array| array << category.id }
+      [create(:category), create(:category)].each_with_object([]) do |category, array|
+        array << category.id
+      end
     end
   end
 
   factory :simple_product_with_categories_attributes, parent: :product, class: Simple do
     type { 'Simple' }
     categories_attributes { [attributes_for(:category)] }
+  end
+
+  factory :composite_product_with_component_ids, parent: :product, class: Composite do
+    type { 'Composite' }
+
+    category_ids do
+      [create(:category), create(:category)].each_with_object([]) do 
+        |category, array| array << category.id
+      end
+    end
+
+    component_ids do
+      [create(:component), create(:component)].each_with_object([]) do |component, array|
+        array << component.id
+      end
+    end
+  end
+
+  factory :composite_product_with_components_attributes, parent: :product, class: Composite do
+    type { 'Composite' }
+    categories_attributes { [attributes_for(:category), attributes_for(:category)] }
+    components_attributes { [attributes_for(:component)] }
   end
 
   trait :product_no_name do
