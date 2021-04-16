d7a11df0828cc0f84561516fabbeb2e7e70e2246
Holden Hinkle <holdenhinkle@gmail.com>
2021-04-15 21:00:35 -0400

    pass product type as argument to shared_examples

--- spec/requests/api/v1/products_request_spec.rb
+++ spec/requests/api/v1/products_request_spec.rb
@@ -43,28 +43,28 @@ RSpec.describe Api::V1::ProductsController, type: :request do
 
   describe '#show' do
     context 'simple product with category ids' do
-      include_examples '#show' do
+      include_examples '#show', 'simple' do
         let!(:url) { url }
         let!(:product) { create(:simple_product_with_category_ids) }  
       end
     end
 
     context 'simple product with category attributes' do
-      include_examples '#show' do
+      include_examples '#show', 'simple' do
         let!(:url) { url }
         let!(:product) { create(:simple_product_with_categories_attributes) }  
       end
     end
 
     context 'composite product with component ids' do
-      include_examples '#show' do
+      include_examples '#show', 'composite' do
         let!(:url) { url }
         let!(:product) { create(:composite_product_with_component_ids) }  
       end
     end
 
     context 'composite product with components attributes' do
-      include_examples '#show' do
+      include_examples '#show', 'composite' do
         let!(:url) { url }
         let!(:product) { create(:composite_product_with_components_attributes) }  
       end
