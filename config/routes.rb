20486d8c291c721aafe95142941a3abb6a5cb247
Holden Hinkle <holdenhinkle@gmail.com>
2021-04-29 20:52:15 -0400

    scope api/v1 to account

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :products
-      resources :categories
-      resources :components
+  scope ':account' do
+    namespace :api do
+      namespace :v1 do
+        resources :products
+        resources :categories
+        resources :components
+      end
     end
   end
 end
