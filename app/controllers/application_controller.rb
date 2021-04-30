d53804c3680d79caee1d8a805e4f2038bd401ceb
Holden Hinkle <holdenhinkle@gmail.com>
2021-04-29 22:22:52 -0400

    fix syntax error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
   private
 
   def set_tenant_by_tenant_id
-    current_account = Account.find_by(tenant_id:: params[:tenant_id])
+    current_account = Account.find_by(tenant_id: params[:tenant_id])
     set_current_tenant(current_account)
   end
 end
