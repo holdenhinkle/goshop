e95a86ca76e6ca21939f1023b6ef06c4aef0fdfb
Holden Hinkle <holdenhinkle@gmail.com>
2021-05-03 20:43:35 -0400

    add routes for acconts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 Rails.application.routes.draw do
-  resources :accounts
-
   scope ':tenant_id' do
     namespace :api do
       namespace :v1 do
+        # account
+        get 'account', to: 'accounts#show'
+        put 'account', to: 'accounts#update'
+        patch 'account', to: 'accounts#update'
+        
         resources :products
         resources :categories
         resources :components
+      end
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      namespace :admin do
+        resources :accounts
       end
     end
   end
