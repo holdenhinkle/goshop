ae5c3edc2499ab348aa2691db3911eaadc23bde2
Holden Hinkle <holdenhinkle@gmail.com>
2021-05-26 21:25:17 -0400

    check if id, tenant_id or slug is a uuid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,18 @@ class ApplicationController < ActionController::API
     current_account = Account.find_by(tenant_id: params[:tenant_id])
     set_current_tenant(current_account)
   end
+
+  def is_uuid?
+    regex = /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
+
+    if params[:id]
+      params[:id].match(regex)
+    elsif params[:slug]
+      params[:slug].match(regex)
+    elsif params[:tenant_id]
+      params[:tenant_id].match(regex)
+    end
+
+    false
+  end
 end
