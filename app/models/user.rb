cc6db2c942b7c96d91632298337fd3af1e94aa3a
Holden Hinkle <holdenhinkle@gmail.com>
2021-05-29 20:18:09 -0400

    set devise routes, user routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  acts_as_tenant :account
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 end
