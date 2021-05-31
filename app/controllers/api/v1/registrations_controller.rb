acfa69ffa7534fa679f31a84a78ab59ed0964a3c
Holden Hinkle <holdenhinkle@gmail.com>
2021-05-31 14:33:04 -0400

    update registrations_controller (wip)

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,21 +1,37 @@
 module Api
   module V1
     class RegistrationsController < Devise::RegistrationsController
-      def create
+
+      def new
+        # do i need the following?
+        # is the check scoped to the tenant_id?
+        render(status: :bad_request) && return if User.exists?(email: sign_up_params['email'])
+
         build_resource(sign_up_params)
         resource.save
         sign_up(resource_name, resource) if resource.persisted?
-        render_user_as_json(resource)
+        render json: resource
+      end
+
+      def create
+        # do i need the following?
+        # is the check scoped to the tenant_id?
+        render(status: :bad_request) && return if User.exists?(email: sign_up_params['email'])
+
+        build_resource(sign_up_params)
+        resource.save
+        sign_up(resource_name, resource) if resource.persisted?
+        render json: resource
       end
 
       private
 
       def sign_up_params
-        params.require(:user).permit(:email, :password)
+        params.require(:user).permit(:email, :password).merge({ account_id: current_tenant.id })
       end
 
       def render_user_as_json(user)
-        render json: UserSerializer.new(user).serializable_hash.to_json        
+        render json: UserRegistrationSerializer.new(user).serializable_hash.to_json        
       end
     end
   end
