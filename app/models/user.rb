50a4cfb3a120f288c42d9913a389f3aa88c32df3
Holden Hinkle <holdenhinkle@gmail.com>
2021-05-31 14:26:38 -0400

    don't use the validatable devise module because email addresses should be scoped to an account. add custom email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ApplicationRecord
+  belongs_to :account
   acts_as_tenant :account
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+  devise :database_authenticatable, :registerable, 
+         :recoverable, :rememberable,
+        #  :confirmable, 
+         :timeoutable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+
+  validates_with EmailAddress::ActiveRecordValidator, field: :email
+  validates :email, presence: true, uniqueness: { scope: :account_id, case_sensitive: false }
+  validates :password, presence: true, length: { :in => 6..20 }, :if => :password_required?
+
+  protected
+
+  def password_required?
+    !persisted? || !password.nil? || !password_confirmation.nil?
+  end
 end
